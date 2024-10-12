3b8674eba6726b7af760e94677b2893c0dd596cd
Giuseppe Zileni <giuseppe.zileni@gmail.com>
2024-10-12 18:19:50 +0200

    updated aws userdata

--- aws/user-data.sh
+++ aws/user-data.sh
@@ -1,45 +1,37 @@
 #!/bin/bash
 
-# export ENV variables
-export ECOSENSOR_PORT=15436
-export ECOSENSOR_OPENMETEO_API=http://ecosensor-open-meteo-api:8080
-export AWS_TOPIC_ARN=
-export AWS_BUCKET_NAME=
-
-# istat container
-export POSTGRES_ISTAT_HOST=localhost
-export POSTGRES_ISTAT_PORT=15434
-export POSTGRES_ISTAT_USER=istat
-export POSTGRES_ISTAT_PASS=
-export POSTGRES_ISTAT_DB=istat
-
-# backend container
-export ECOSENSOR_PORT=15435
-export ECOSENSOR_OPENMETEO_API=http://ecosensor-open-meteo-api:8080
-export AWS_TOPIC_ARN=
-
-# database
-export POSTGRES_HOST=localhost
-export POSTGRES_PORT=15432
-export POSTGRES_USER=ecosensor
-export POSTGRES_PASS=
-export POSTGRES_DB=ecosensor
-export POSTGRES_SCHEMA=public
-
-# open-meteo
-export OPEN_METEO_MODELS=ncep_gfs013,copernicus_era5,copernicus_dem90,cams_europe
-export OPEN_METEO_VARIABLES=temperature_2m,precipitation,carbon_monoxide,nitrogen_dioxide,ozone,pm10,pm2_5
-export OPEN_METEO_MAX_AGE_DAYS=3
-export OPEN_METEO_REPEAT_INTERVAL=5
-export OPEN_METEO_CONCURRENT=4
-export OPEN_METEO_PORT=15434
-
-# Database Osm
-export POSTGRES_OSM_HOST=localhost
-export POSTGRES_OSM_PORT=15432
-export POSTGRES_OSM_USER=osm
-export POSTGRES_OSM_PASS=
-export POSTGRES_OSM_DB=osm
+# Create .env file in the home directory
+cat <<EOF > ~/.env
+ECOSENSOR_PORT=15436
+ECOSENSOR_OPENMETEO_API=http://ecosensor-open-meteo-api:8080
+AWS_TOPIC_ARN=
+AWS_BUCKET_NAME=
+POSTGRES_ISTAT_HOST=localhost
+POSTGRES_ISTAT_PORT=15434
+POSTGRES_ISTAT_USER=istat
+POSTGRES_ISTAT_PASS=
+POSTGRES_ISTAT_DB=istat
+ECOSENSOR_PORT=15435
+ECOSENSOR_OPENMETEO_API=http://ecosensor-open-meteo-api:8080
+AWS_TOPIC_ARN=
+POSTGRES_HOST=localhost
+POSTGRES_PORT=15432
+POSTGRES_USER=ecosensor
+POSTGRES_PASS=
+POSTGRES_DB=ecosensor
+POSTGRES_SCHEMA=public
+OPEN_METEO_MODELS=ncep_gfs013,copernicus_era5,copernicus_dem90,cams_europe
+OPEN_METEO_VARIABLES=temperature_2m,precipitation,carbon_monoxide,nitrogen_dioxide,ozone,pm10,pm2_5
+OPEN_METEO_MAX_AGE_DAYS=3
+OPEN_METEO_REPEAT_INTERVAL=5
+OPEN_METEO_CONCURRENT=4
+OPEN_METEO_PORT=15434
+POSTGRES_OSM_HOST=localhost
+POSTGRES_OSM_PORT=15432
+POSTGRES_OSM_USER=osm
+POSTGRES_OSM_PASS=
+POSTGRES_OSM_DB=osm
+EOF
 
 sudo yum update
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo 
@@ -58,8 +50,8 @@ sudo systemctl enable docker.service
 sudo systemctl start docker.service
 
 # Clone repository and build stack
-git clone https://github.com/EMP-Projects/EcoSensor-Stack.git /EcoSensor
-cd EcoSensor
+git clone https://github.com/EMP-Projects/EcoSensor-Stack.git ~/ecosensor-stack
+cd ecosensor-stack
 
 docker-compose rm -f
 docker-compose pull
