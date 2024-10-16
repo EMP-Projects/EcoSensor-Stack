71527f09681ef27500268e8e5ecda0615dbe235d
Giuseppe Zileni <giuseppe.zileni@gmail.com>
2024-10-16 12:43:21 +0200

    updated scripts aws

--- aws/pg-install.sh
+++ aws/pg-install.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-pg_pass=$(aws ssm get-parameter --with-decryption --profile default --name "ECOSENSOR_PG_PASS" --query "Parameter.Value" --output text) 
-pg_pass_osm=$(aws ssm get-parameter --with-decryption --profile default --name "ECOSENSOR_OSM_PASS" --query "Parameter.Value" --output text) 
-pg_pass_istat=$(aws ssm get-parameter --with-decryption --profile default --name "ECOSENSOR_ISTAT_PASS" --query "Parameter.Value" --output text) 
-
 # Database
 export POSTGRES_USER=ecosensor
-export POSTGRES_PASS=$pg_pass
+export POSTGRES_PASS=$(aws ssm get-parameter --with-decryption --profile default --name "ECOSENSOR_PG_PASS" --query "Parameter.Value" --output text)
 export POSTGRES_DB=ecosensor
 
 # Database Istat
 export POSTGRES_ISTAT_USER=istat
-export POSTGRES_ISTAT_PASS=$pg_pass_istat
+export POSTGRES_ISTAT_PASS=$(aws ssm get-parameter --with-decryption --profile default --name "ECOSENSOR_ISTAT_PASS" --query "Parameter.Value" --output text)
 export POSTGRES_ISTAT_DB=istat
 
 # Database Osm2pgsql
 export POSTGRES_OSM_USER=osm
-export POSTGRES_OSM_PASS=$pg_pass_osm
+export POSTGRES_OSM_PASS=$(aws ssm get-parameter --with-decryption --profile default --name "ECOSENSOR_OSM_PASS" --query "Parameter.Value" --output text) 
 export POSTGRES_OSM_DB=osm
 
 # -----------------------------------
