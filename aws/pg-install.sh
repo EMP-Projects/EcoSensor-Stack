6a45558fb5374670abd8f8922b2e02c1ab98cca9
Giuseppe Zileni <giuseppe.zileni@gmail.com>
2024-10-16 12:48:15 +0200

    updated scripts aws

--- aws/pg-install.sh
+++ aws/pg-install.sh
@@ -2,17 +2,17 @@
 
 # Database
 export POSTGRES_USER=ecosensor
-export POSTGRES_PASS=$(aws ssm get-parameter --with-decryption --profile default --name "ECOSENSOR_PG_PASS" --query "Parameter.Value" --output text)
+export POSTGRES_PASS=$(aws ssm get-parameter --with-decryption --region us-east-1 --profile default --name "ECOSENSOR_PG_PASS" --query "Parameter.Value" --output text)
 export POSTGRES_DB=ecosensor
 
 # Database Istat
 export POSTGRES_ISTAT_USER=istat
-export POSTGRES_ISTAT_PASS=$(aws ssm get-parameter --with-decryption --profile default --name "ECOSENSOR_ISTAT_PASS" --query "Parameter.Value" --output text)
+export POSTGRES_ISTAT_PASS=$(aws ssm get-parameter --with-decryption --region us-east-1 --profile default --name "ECOSENSOR_ISTAT_PASS" --query "Parameter.Value" --output text)
 export POSTGRES_ISTAT_DB=istat
 
 # Database Osm2pgsql
 export POSTGRES_OSM_USER=osm
-export POSTGRES_OSM_PASS=$(aws ssm get-parameter --with-decryption --profile default --name "ECOSENSOR_OSM_PASS" --query "Parameter.Value" --output text) 
+export POSTGRES_OSM_PASS=$(aws ssm get-parameter --with-decryption --region us-east-1 --profile default --name "ECOSENSOR_OSM_PASS" --query "Parameter.Value" --output text) 
 export POSTGRES_OSM_DB=osm
 
 # -----------------------------------
@@ -21,17 +21,17 @@ sudo apt update
 sudo apt install -y postgresql postgresql-client postgis
 
 # create user and database
-sudo -u postgres psql -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASS';"
-sudo -u postgres psql -c "CREATE DATABASE $POSTGRES_DB OWNER $POSTGRES_USER;"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;"
+sudo -u ubuntu psql -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASS';"
+sudo -u ubuntu psql -c "CREATE DATABASE $POSTGRES_DB OWNER $POSTGRES_USER;"
+sudo -u ubuntu psql -c "GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;"
 
-sudo -u postgres psql -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_OSM_PASS';"
-sudo -u postgres psql -c "CREATE DATABASE $POSTGRES_OSM_DB OWNER $POSTGRES_OSM_USER;"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_OSM_DB TO $POSTGRES_OSM_USER;"
+sudo -u ubuntu psql -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_OSM_PASS';"
+sudo -u ubuntu psql -c "CREATE DATABASE $POSTGRES_OSM_DB OWNER $POSTGRES_OSM_USER;"
+sudo -u ubuntu psql -c "GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_OSM_DB TO $POSTGRES_OSM_USER;"
 
-sudo -u postgres psql -c "CREATE USER $POSTGRES_ISTAT_USER WITH PASSWORD '$POSTGRES_ISTAT_PASS';"
-sudo -u postgres psql -c "CREATE DATABASE $POSTGRES_ISTAT_DB OWNER $POSTGRES_ISTAT_USER;"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_ISTAT_DB TO $POSTGRES_ISTAT_USER;"
+sudo -u ubuntu psql -c "CREATE USER $POSTGRES_ISTAT_USER WITH PASSWORD '$POSTGRES_ISTAT_PASS';"
+sudo -u ubuntu psql -c "CREATE DATABASE $POSTGRES_ISTAT_DB OWNER $POSTGRES_ISTAT_USER;"
+sudo -u ubuntu psql -c "GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_ISTAT_DB TO $POSTGRES_ISTAT_USER;"
 
 # add extension postgis to the database
 sudo -u ubuntu psql -c "CREATE EXTENSION IF NOT EXISTS postgis;" -d $POSTGRES_OSM_DB
