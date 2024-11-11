be8f8a4895464bf9c7668e1e45c55932962ceacb
Giuseppe Zileni <giuseppe.zileni@gmail.com>
2024-11-11 10:16:38 +0100

    updated scripts aws

--- aws/cp_scripts.bat
+++ aws/cp_scripts.bat
@@ -3,26 +3,32 @@ setlocal
 
 REM Controlla se è stato passato un parametro per SOURCE_FILE
 if "%~1"=="" (
-    echo Usage: %0 source_file host
+    echo Usage: %0 source_file host key
     exit /b 1
 )
 
 if "%~2"=="" (
-    echo Usage: %0 source_file host
+    echo Usage: %0 source_file host key
+    exit /b 1
+)
+
+if "%~3"=="" (
+    echo Usage: %0 source_file host key
     exit /b 1
 )
 
 set "SOURCE_FILE=%~1"
 set "HOST=%~2"
+set "PEM_FILE=%~3"
 
-set "PEM_FILE=C:\Users\gzile\OneDrive\keys\ecosensor-ssh.pem"
+set "PEM_PATH=C:\Users\gzile\OneDrive\keys\%PEM_FILE%.pem"
 
 REM Configura le variabili
 set "USER=admin"
 set "DESTINATION_PATH=/home/admin/"
 
 REM Copia il file utilizzando scp
-scp -i "%PEM_FILE%" "%SOURCE_FILE%" "%USER%@%HOST%:%DESTINATION_PATH%"
+scp -i "%PEM_PATH%" "%SOURCE_FILE%" "%USER%@%HOST%:%DESTINATION_PATH%"
 
 endlocal
 
